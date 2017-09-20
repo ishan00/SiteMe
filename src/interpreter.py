2e0103344a300fda754fd2b769d8e42b0ce38b21
ishan00 <ishantarunesh@gmail.com>
2017-09-20 16:35:05 +0530

    Added Icons, Directory Made

---
+++ src/interpreter.py
@@ -0,0 +1,43 @@
+def titleLine(str):
+	return (str.find('title') != -1)
+def styleLine(str):
+	return (str.find('font') != -1)
+
+config_file=open('config.txt')
+lines = config_file.readlines()
+l1 = [line.strip('\n') for line in lines]
+
+page_file = open('./pages/index.txt')
+l2 = [line.strip('\n') for line in page_file.readlines()]
+
+#print(l1)
+#print(l2)
+
+print ("""
+<html>
+<head>
+""")
+
+for temp_line in l2:
+	if(titleLine(temp_line)):
+		print ("<title>" + temp_line[temp_line.find(':')+1:] + "</title>")
+	elif(styleLine(temp_line)):
+		print ("<style>")
+		print ("body{")
+		print ("font-size:" + temp_line[temp_line.find(':')+1:])
+		print ("}")
+		print ("</style>")
+
+print ("""
+</head>
+<body>
+""")
+
+for temp_line in l1:
+	if(temp_line.find('navbar') != -1):
+		print(temp_line[6:])
+
+print ("""
+</body>
+</html>
+""")
