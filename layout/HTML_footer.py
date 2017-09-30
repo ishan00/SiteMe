9246a98ccf008ac811d28347f8f3d5c50b4e92c8
ishan00 <ishantarunesh@gmail.com>
2017-09-30 20:01:38 +0530

    Converted Footers to Dictionaries

---
+++ layout/HTML_footer.py
@@ -0,0 +1,105 @@
+#type:social
+footer_social = \
+{'footer':{'id':'myFooter'} , 'content':{ 
+	1:{'div':{'class':'footer-social'} , 'content': { 
+		1:{ 'a':{'href':'', 'class':'social-icons'} , 'content': { 
+			1:{'i':{'class':'fa fa-facebook'} , 'content':''}}}, 
+		2:{ 'a':{'href'='', 'class':'social-icons'} , 'content': { 
+			1:{'i':{'class':'fa fa-google-plus'} , 'content':''}}} , 
+		3:{ 'a':{'href'='', 'class':'social-icons'} , 'content': { 
+			1:{'i':{'class':'fa fa-twitter'} , 'content':''}}}}}}}
+
+#type:basic
+footer_basic = \
+{'footer':{'class':'footer-basic-centered'},'content':{
+	1:{'p':{'class':'footer-company-motto'},'content':''},
+	2:{'p':{'class':'footer-links'}, 'content':{
+		1:{'a':{'href':''} , 'content':''}}} ,
+	3:{'p':{'class':'footer-company-name'},'content':''}}}
+
+#type:distributed
+footer_distributed= \
+{'footer':{'class':'footer-distributed'},'content':{
+	1:{'div':{'class':'footer-right'},'content': {
+		1:{ 'a':{'href':''} , 'content': {
+			1:{'i':{'class':'fa fa-facebook'} , 'content':''}}},
+		2:{ 'a':{'href'=''} , 'content': {
+			1:{'i':{'class':'fa fa-google-plus'} , 'content':''}}} , 
+		3:{ 'a':{'href'=''} , 'content': {
+			1:{'i':{'class':'fa fa-twitter'} , 'content':''}}} ,
+		4:{ 'a':{'href'=''} , 'content': {
+			1:{'i':{'class':'fa fa-github'} , 'content':''}}}}} , 
+	2:{'div':{'class':'footer-left'}, 'content':{
+		1:{'p':{'class':'footer-links'} , 'content':{ 
+			1:{'a':{'href':''} , 'content':'' }}}}}}}
+
+#type:distributed_phone_address
+footer_distributed_phone_address = \
+{'footer':{'class':'footer-distributed'} , 'content':{ 
+	1:{'div':{'class':'footer-left'} , 'content':{ 
+		1:{'h3':{} , 'content':''} , 
+		2:{'p':{'class':'footer-links'}, 'content':{
+			1:{'a':{'href':''} , 'content':''}}} , 
+		3:{'p':{'class':'footer-company-name'},'content':''}}}
+	2:{'div':{'class':'footer-center'} , 'content':{
+		1:{'div':{},'content':{
+			1:{'i':{'class':'fa fa-map-marker'} , 'content':''}
+			2:{'p':{}, 'content':''}}
+		}
+		2:{'div':{},'content':{
+			1:{'i':{'class':'fa fa-phone'} , 'content':''}
+			2:{'p':{}, 'content':''}}
+		}
+		3:{'div':{},'content':{
+			1:{'i':{'class':'fa fa-envelope'} , 'content':''}
+			2:{'p':{}, 'content':''}}
+		}
+		}
+	}
+	3:{'div':{'class':'footer-right'} , 'content':{
+		1:{'p':{'class':'footer-company-about'} , 'content':''}
+		2:{'div':{'class':'footer-icons'},'content': {
+			1:{ 'a':{'href'=''} , 'content': { 
+				1:{'i':{'class':'fa fa-facebook'},'content':''}}}, 
+			2:{ 'a':{'href'=''} , 'content': { 
+				1:{'i':{'class':'fa fa-google-plus'},'content':''}}},
+			3:{ 'a':{'href'=''} , 'content': { 
+				1:{'i':{'class':'fa fa-twitter'},'content':''}}},
+			4:{ 'a':{'href'=''} , 'content': {
+				1:{'i':{'class':'fa fa-github'},'content':''}}}}}}}}}
+
+#type:distributed_contact_form
+footer_distributed_contact = \
+{'footer':{'class':'footer-distributed'} , 'content':{ 
+	1:{'div':{'class':'footer-left'} , 'content':{ 
+		1:{'h3':{} , 'content':''} , 
+		2:{'p':{'class':'footer-links'}, 'content':{
+			1:{'a':{'href':''} , 'content':''}}} , 
+		3:{'p':{'class':'footer-company-name'},'content':''}
+		4:{'div':{'class':'footer-icons'},'content': {
+			1:{ 'a':{'href'=''} , 'content': { 
+				1:{'i':{'class':'fa fa-facebook'},'content':''}}}, 
+			2:{ 'a':{'href'=''} , 'content': { 
+				1:{'i':{'class':'fa fa-google-plus'},'content':''}}},
+			3:{ 'a':{'href'=''} , 'content': { 
+				1:{'i':{'class':'fa fa-twitter'},'content':''}}},
+			4:{ 'a':{'href'=''} , 'content': {
+				1:{'i':{'class':'fa fa-github'},'content':''}}}}}}}
+	2:{'div':{'class':'footer-right'} , 'content':{
+		1:{'p':{'class':'footer-company-about'} , 'content':''}
+		2:{'form':{'action':'#' , 'method'='post'} , 'content':{
+			1:{'input':{'type':'text' ,'name':'email' 'placeholder':'Email'} , 'content':''}
+			2:{'textarea':{'name':'message' , placeholder'Message'}, 'content':''}
+			3:{'button':{}, 'content':'Send'}}}}}}}
+	
+#type:distributed_search
+footer_distributed_search = \
+{'div':{'class':'footer-distributed'} , 'content':{
+	1:{'div':{'class':'footer-links'} , 'content':{
+		1:{'p':{'class':'footer-links'}, 'content':{
+			1:{'a':{'href':''} , 'content':''}}} , 
+		2:{'p':{'class':'footer-company-name'},'content':''}}} , 
+	2:{'div':{'class':'footer-right'} , 'content':{
+		1:{'form'{'method':'get' ,'action':'#'} , 'content':{
+			1:{'input':{'placeholder':'Search our website', 'name':'search'}} ,
+			2:{'i':{'class':'fa fa-search'} , 'content':''}}}}}}}
