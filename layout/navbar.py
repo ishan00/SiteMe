c673938d3d59da05ab695cb42fdc955b1f5fb04e
ishan00 <ishantarunesh@gmail.com>
2017-09-30 16:36:46 +0530

    Converted Navbars to Dictionaries

---
+++ layout/navbar.py
@@ -0,0 +1,22 @@
+# This file is storehouse for all dictionaries of navbar layout. You can find the type of navbar in comments
+
+#type:orange
+navbar_orange = {'div':{'id':'cssmenu'}, 'content':{ 1:{'ul':{} , 'content':{ 1:{'li':{'class':'active'}, 'content':{ 1:{'a':{'href':''} ,'content':'' } } } } } } }
+
+#type:flat
+navbar_flat = {'div':{'id':'cssmenu'}, 'content':{ 1:{'ul':{} , 'content':{ 1:{'li':{'class':'active'}, 'content':{ 1:{'a':{'href':''} ,'content':'' } } } } } } }
+
+#type:indented
+navbar_indented = {'div':{'id':'cssmenu'}, 'content':{ 1:{'ul':{} , 'content':{ 1:{'li':{'class':'active'}, 'content':{ 1:{'a':{'href':''} ,'content':'' } } } } } } }
+
+#type:wrap
+navbar_wrap = {'div':{'class':'wrap'} , 'content':{ 1:{'span':{'class':'decor'} , 'content':''} , 2:{'nav':{} , 'content':{ 1:{'ul':{'class':'primary'} , 'content':{ 1:{'li':{} , 'content':{ 1:{'a':{'href':''} , 'content':''} , 2:{'ul':{'class':'sub'} , 'content' : {1:{'li':{'href':''} , 'content':''} } } } } } } } } } }
+
+#type:toggle
+navbar_toggle = {'header':{} , 'content':{ 1:{'button':{'class':'toggle-nav'} , 'content':{ 1:{'span':{} , 'content':'>' } } } , 2:{'ul':{'class':'nav'} , 'content':{ 1:{'li':{'class':'loud'} , 'content':{'a':{'href'=''} , 'content':''} } } } } }
+
+#type:open
+navbar_open = {'div':{'class':'open'} , 'content':{ 1:{'span':{'class':'cls'} , 'content':''} , 2:{'span':{} , 'content':{ 1:{'ul':{'class':'sub-menu'} , 'content':{ 1:{'li':{} , 'content':{ 1:{'a':{'href':''} , 'content':''} } } } } } } } }
+
+#type:breadcrumbs
+navbar_breadcrumbs = {'div':{'class':'breadcrumbs'} , 'content':{ 1:{'div':{'class':'inner'} , 'content':{ 1:{'ul':{'class':'cf'} , 'content':{ 1:{'li':{} , 'content':{ 1:{'a':{'href':'' , 'class':'active'} , 'content':{ 1: {'span':{}, 'content':''} } } } } } } } } } }
